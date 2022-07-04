ee8a3b3f648098d1deaf90c40da944cb64a749ac
Oldrich Smehlik <oldrich@smehlik.net>
2022-07-04 10:12:14 +0200

    Refactor add-or-edit-tags

--- metasearcher.py
+++ metasearcher.py
@@ -4,13 +4,39 @@ import os
 import subprocess
 
 
-def get_files():
+def get_selected_files():
     """
     Returns list of selected files in Nautilus.
     """
     selected_file_paths = os.environ["NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"]
     selected_file_paths = selected_file_paths.strip().split("\n")
     return selected_file_paths
+
+
+def get_metadata_file_path(data_file_path: str) -> str:
+    """
+    Returns path to metadata file for the given data file.
+    """
+    return data_file_path + ".meta"
+
+
+def load_metadata(metadata_file_path: str) -> dict:
+    """
+    Loads metadata from file.
+    """
+    if os.path.exists(metadata_file_path):
+        with open(metadata_file_path, "r", encoding="utf8") as f:
+            return json.load(f)
+    else:
+        return {}
+
+
+def save_metadata(metadata: dict, metadata_file_path: str) -> None:
+    """
+    Saves metadata to file.
+    """
+    with open(metadata_file_path, "w", encoding="utf8") as f:
+        json.dump(metadata, f)
 
 
 def find_metadata_files_in_directory(path_to_directory):
@@ -48,3 +74,4 @@ def show_gui_add_edit_tags(old_tags=[]):
     stdout = completed_process.stdout.decode("utf8").strip()
     new_tags = stdout.split(" ")
     return new_tags
+
